292521369a175ce81fc62c1fdeabe011f3a005bf
abdulrahman nur <abdulrahman.khalif.nur@gmail.com>
2025-08-01 21:54:31 +0300

    Implement authentication and integrate Puter API

---
+++ app/routes/auth.tsx
@@ -0,0 +1,64 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+
+export const meta = () => {
+  return [
+    { title: "CVision – AI Resume Analyzer" },
+    {
+      name: "description",
+      content:
+        "Login to your account to track your applications and resume ratings.",
+    },
+  ];
+};
+
+const auth = () => {
+  const { isLoading, auth } = usePuterStore();
+  const location = useLocation();
+  const next = location.search.split("next=")[1];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.isAuthenticated) {
+      navigate(next);
+    }
+  }, [auth.isAuthenticated, next]);
+
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+      <div className="gradient-border shadow-lg">
+        <section className="flex flex-col gap-8 bg-white rounded-2xl p-10 ">
+          <div className="flex flex-col items-center text-center gap-4">
+            <h1 className="text-3xl font-bold">Welcome</h1>
+            <h2 className="text-lg">
+              Login to your account to track your applications and resume
+              ratings.
+            </h2>
+          </div>
+          <div className="flex flex-col gap-4 text-center items-center">
+            {isLoading ? (
+              <button className="auth-button animate-pulse">
+                <p>Signing you in ...</p>
+              </button>
+            ) : (
+              <>
+                {auth.isAuthenticated ? (
+                  <button className="auth-button" onClick={auth.signOut}>
+                    <p>Log Out</p>
+                  </button>
+                ) : (
+                  <button className="auth-button" onClick={auth.signIn}>
+                    <p>Log In</p>
+                  </button>
+                )}
+              </>
+            )}
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default auth;
